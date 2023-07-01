b1c3551da561920759bbc149424c686a060894cb
SHIVAM SINGH <91870115+shivam8989@users.noreply.github.com>
2023-07-01 10:38:28 +0530

    Add files via upload
    Type Conversion and Type Casting in java

---
+++ Basics.java
@@ -0,0 +1,13 @@
+public class Basics {
+    public static void main(String args[]) {
+        //  Type conversion
+        int a = 2;
+        long b = a;
+        System.out.println(b);
+
+
+        float v = 99.9f;
+        int d = (int) (v);
+        System.out.println(d);
+    }
+}
